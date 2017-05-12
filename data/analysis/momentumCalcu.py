cb1dfe66a3f3dbb6fb65d68e7d977ea4de73d1d7
Emilio <emiliocosy@163.com>
2017-05-12 15:20:18 +0800

    v0.2 classify model

--- data/analysis/momentumCalcu.py
+++ data/analysis/momentumCalcu.py
@@ -21,11 +21,13 @@ for i in range(1, len(dataset)):
 print("-------------------- Momentum Distribution ------------------")
 print(dataset['mmt'].value_counts(bins=5).sort_index())
 
+pd.set_option('mode.chained_assignment',None)
 # classify by counts
 dataset['label'] = 0
-
+# read only
+mmt_series = dataset['mmt']
 for i in range(len(dataset)):
-    mmt = dataset['mmt'][i]
+    mmt = mmt_series[i]
     if mmt < -0.02:
         dataset['label'][i] = 0
     elif mmt < -0.005:
@@ -36,15 +38,23 @@ for i in range(len(dataset)):
         dataset['label'][i] = 3
     else:
         dataset['label'][i] = 4
+print("---- Label Distribution Check --------")
+print(dataset['label'].value_counts().sort_index())
+# simple way
+# dataset.loc[:,'label'][mmt_series < -0.02] = 0
+# dataset.loc[:,'label'][mmt_series > 0.02] = 4
+# dataset.loc[:,'label'][(mmt_series >= -0.02) & (mmt_series < -0.005)] = 1
+# dataset.loc[:,'label'][(mmt_series >= -0.005) & (mmt_series <= 0.005)] = 2
+# dataset.loc[:,'label'][(mmt_series > 0.005) & (mmt_series <= 0.02)] = 3
 
 # EXCEPTION WARNNING: SettingWithCopyWarning (Pandas don't know whether a copy or view return, no guarantee of successful write)
+# print(dataset)
+# dataset['label'].plot(lw = 2.)
+# plt.show()
 
-dataset['label'].plot(lw = 2.)
-plt.show()
-
-# dataset['mmt'].value_counts(bins=10, ascending=False).plot(lw = 2.)
-plt.hist(dataset['mmt'].values, bins = 5)
-plt.xlabel('Momentum')
-plt.ylabel('Counts')
-# plt.grid(True)
-plt.show()
+# # dataset['mmt'].value_counts(bins=10, ascending=False).plot(lw = 2.)
+# plt.hist(dataset['mmt'].values, bins = 5)
+# plt.xlabel('Momentum')
+# plt.ylabel('Counts')
+# # plt.grid(True)
+# plt.show()
--- model/classifier.py
+++ model/classifier.py
@@ -6,22 +6,53 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import time
 
+######################################
+####### Data Preparation #############
+######################################
 data_file = "../data/dataset/close_2016-2017.csv"
 dataset = pd.read_csv(data_file,index_col=0, sep=',', usecols=[0,1], skiprows=1, names=['date','close'],parse_dates=True)
 print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , " ------ Complete Data Reading")
-# how long will a span cover, e.g. 20 days
-TIME_SPAN = 20
-TRAIN_RATIO = 0.9#0.8
-BATCH_SIZE = 3
 
-# y batch is just 1 step further into the future than X batch
+# calculate momentum: Mt = (CLOSE(t) -CLOSE(t-1))/CLOSE(t)
+dataset['mmt'] = 0.0
+for i in range(1, len(dataset)):
+    dataset['mmt'][i] = (dataset['close'][i] - dataset['close'][i-1]) / dataset['close'][i]
+pd.set_option('mode.chained_assignment',None)
+# classify by counts
+dataset['label'] = 0
+mmt_series = dataset['mmt']
+for i in range(len(dataset)):
+    mmt = mmt_series[i]
+    if mmt < -0.02:
+        dataset['label'][i] = 0
+    elif mmt < -0.005:
+        dataset['label'][i] = 1
+    elif mmt < 0.005:
+        dataset['label'][i] = 2
+    elif mmt < 0.02:
+        dataset['label'][i] = 3
+    else:
+        dataset['label'][i] = 4
+print("---- Label Distribution Check --------")
+print("Total: ",len(dataset['label']),dataset['label'].value_counts().sort_index())
+print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , " ------ Complete Data Preparation")
+
+
+# Batch Generator. ps: y batch is mmt classification
 def next_batch(n_batch, n_step,n_input, index=0):
     X_batch, y_batch = [],[]
     for i in range(index, n_batch+index):
         X_batch.append(dataset['close'].values[i : i+n_step])
-        y_batch.append(dataset['close'].values[i+1 : i+1+n_step])
+        y_batch.append(dataset['label'].values[i : i+n_step])
     return np.array(X_batch).reshape(n_batch,n_step,n_input), np.array(y_batch).reshape(n_batch,n_step,n_input)
 
+
+# how long will a span cover, e.g. 20 days (4 tradable weeks)
+TIME_SPAN = 20
+TRAIN_RATIO = 0.9#0.8
+BATCH_SIZE = 3
+
+# testing data correctness
 for i in range(1):
     X_batch, y_batch = next_batch(BATCH_SIZE, TIME_SPAN, 1, i*TIME_SPAN*BATCH_SIZE)
 
@@ -47,20 +78,30 @@ lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons, activation=tf.nn.r
 cells = lstm_cell
 # 3 layers' lstm
 #cells = tf.contrib.rnn.MultiRNNCell([lstm_cell]*3)
-#rnn_outputs, states = tf.nn.dynamic_rnn(multi_cell, X, dtype=tf.float32)
+
 # Add Dropout
 is_training = False 
 keep_prob = 0.75
 if is_training:
     lstm_cell = tf.contrib.rnn.DropoutWrapper(cells, input_keep_prob=keep_prob)
+
 rnn_outputs, states = tf.nn.dynamic_rnn(cells, X, dtype=tf.float32)
-stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
-stacked_outputs = fully_connected(stacked_rnn_outputs, n_output, activation_fn=None)
-outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_output])
-# define loss optimizer
-loss = tf.reduce_sum(tf.square(outputs-y))
-optimizer = tf.train.AdamOptimizer(learning_rate)
+classifier= fully_connected(states, n_outputs, activation_fn=None)
+print("Shape of Outputs: ",outputs.shape, "shape of states: ", states.shape)
+print("Shape of classifier: ",classifier.shape)
+# stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
+# stacked_outputs = fully_connected(stacked_rnn_outputs, n_output, activation_fn=None)
+# outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_output])
+
+# softmax + cross entropy calculation
+xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=classifier)
+# define loss function & optimize method
+loss = tf.reduce_mean(xentropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(loss)
+# measurement
+correct = tf.nn.in_top_k(classifier, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
 def elapsed(sec):
     if sec<60:
@@ -73,14 +114,13 @@ def elapsed(sec):
 ##############################
 ##### Training Phase #########
 ##############################
-n_epoch = 10 
+n_epoch = 10
 n_input = 1
 data_size = len(dataset['close']) // BATCH_SIZE
 n_training = int(data_size * TRAIN_RATIO)
 print("data size %d, train size %d" % (data_size,n_training))
 
-# add a test case
-
+# add a test case //SHOULD BE MORE
 size = len(dataset.index)
 data = dataset['close'].values[size-TIME_SPAN-1:]
 X_new = np.array(data[:-1])
@@ -99,20 +139,26 @@ with tf.Session() as sess:
             X_batch, y_batch = next_batch(BATCH_SIZE, TIME_SPAN,n_input, i*BATCH_SIZE)
             sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
             if i % 10 == 0:
-                mse = loss.eval(feed_dict={X: X_batch, y: y_batch})
-                print(i,elapsed(time.time()-start_time), "MSE: ", mse)
+                acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+                acc_test = accuracy.eval(feed_dict={X: X_test,y: y_test})
+                print(i,elapsed(time.time()-start_time), "Train accuracy: ", acc_train, "Test accuracy:", acc_test)
         # evaluate
+        print("---------Epoch ", j, " Train accuracy:", acc_train, " Test accuracy:", acc_test)
         # X_new = time_series(np.array(t_instance[:-1].reshape(-1, n_steps, n_input)))
         y_pred = sess.run(outputs, feed_dict={X: X_test})
         #print(y_pred[0,:-1,0],'\n',X_test[0,1:,0])
     #print(y_pred)
     #y_target = time_series(np.array(t_instance[1:].reshape(-1, n_steps, n_input)))
     #print(y_target)
-
+y_predict_series = []
+y_pred = y_pred[0,:,0]
+categoryList = [-0.04, -0.01, 0, 0.01, 0.04] # rough category value
+for i in range(len(y_pred)):
+    y_predict_series.append(X_new[i] * categoryList[y_pred[i]] + X_new[i])
 plt.title("Testing the model", fontsize=14)
 plt.plot(date_axis[:-1], X_new, "bo", markersize=10, label="instance")
 plt.plot(date_axis[1:], y_new, "y*", markersize=10, label="target")
-plt.plot(date_axis[1:], y_pred[0,:,0], "r.", markersize=10, label="prediction")
+plt.plot(date_axis[1:], y_predict_series, "r.", markersize=14, label="prediction")
 plt.legend(loc="upper left")
 plt.xlabel("Time")
 plt.show()
