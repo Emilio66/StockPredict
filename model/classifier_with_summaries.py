3952301fb966becdc49fba80161a1f2fd7d203cd
Emilio <1005874540@qq.com>
2017-05-26 11:56:17 +0800

    v0.5 change input dim. & topK measurement

--- model/classifier_with_summaries.py
+++ model/classifier_with_summaries.py
@@ -24,8 +24,8 @@ FLAGS = None
 ####### Data Preparation ########
 #################################
 def data_prepare():
-	data_file = "../data/dataset/close_2016-2017.csv"
-	#data_file = "../data/dataset/close_2012-2017.csv"
+	#data_file = "../data/dataset/close_2016-2017.csv"
+	data_file = "../data/dataset/close_2012-2017.csv"
 	dataset = pd.read_csv(data_file,index_col=0, sep=',', usecols=[0,1], skiprows=1, names=['date','close'],parse_dates=True)
 	print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , " ------ Complete Data Reading")
 
@@ -39,13 +39,13 @@ def data_prepare():
 	mmt_series = dataset['mmt']
 	for i in range(len(dataset)):
 		mmt = mmt_series[i]
-		if mmt < -0.02:
+		if mmt < -0.03: #-0.02:
 		    dataset['label'][i] = 0
 		elif mmt < -0.005:
 		    dataset['label'][i] = 1
 		elif mmt < 0.005:
 		    dataset['label'][i] = 2
-		elif mmt < 0.02:
+		elif mmt < 0.03: #0.02:
 		    dataset['label'][i] = 3
 		else:
 		    dataset['label'][i] = 4
@@ -84,19 +84,19 @@ def train(dataset):
 	tf.reset_default_graph()
 
 	# how long will a span cover, e.g. 20 days (4 tradable weeks)
-	TIME_SPAN = 10
+	TIME_SPAN = 4 # 4weeks, 1month
 	TRAIN_RATIO = 0.9#0.8
-	BATCH_SIZE = 3 
+	BATCH_SIZE = FLAGS.batch_size
 	n_neurons = 150 #250
 	n_steps = TIME_SPAN
-	n_input = 1
+	n_input = 5#1, 1 week's data as feature
 	n_output = 5 #5 class
 	n_layers = 3 #5#10 #5 #3
 	learning_rate = 0.001#0.0005 # # 0.02 # 0.005
 
 	with tf.name_scope('input'):
 		X = tf.placeholder(tf.float32, [None, n_steps, n_input], name='X-input')
-		y = tf.placeholder(tf.int32, [None], name='y-input') # one sequence one label
+		y = tf.placeholder(tf.int32, [None, n_steps], name='y-input') # one sequence one label
 
 	
 	with tf.variable_scope("lstm_layers", initializer=tf.contrib.layers.variance_scaling_initializer()):
@@ -117,7 +117,7 @@ def train(dataset):
 	tf.summary.histogram('lstm_outputs',rnn_outputs) # new_h, every output
 	tf.summary.histogram('lstm_states', states)  # state is just the tuple(new_c, new_h)
 
-	states = states[-1][1] #retrieve the last state tuple and only need last output/hypothesis states, omit memory cell
+	states = states[-1][1] #retrieve the last layer's state tuple and only need last output/hypothesis states, omit memory cell
 	print("Final STATES: ", states)
 
 	tf.summary.histogram('lstm_cell_states', states)
@@ -125,11 +125,13 @@ def train(dataset):
 	#tf.summary.histogram('lstm_cell_states_plus_bias', states)
 
 	#with tf.name_scope('fully_connected_layer'):
-	fc_layer = fully_connected(states, n_output, activation_fn=None)
+	#fc_layer = fully_connected(states, n_output, activation_fn=None)
+	fc_layer = fully_connected(rnn_outputs, n_output, activation_fn=None)
 
-	print("Shape of Outputs: ",rnn_outputs, "shape of states: ", states)
+	print("RNN Outputs: ",rnn_outputs, " RNN states: ", states) 	# Output [batch_size, n_steps, n_neurons], States[batch_size, n_neurons]
 	print("Shape of Outputs: ",rnn_outputs.shape, "shape of states: ", states.shape)
-	print("Shape of fc_layer: ",fc_layer.shape, "Shape of y: ", y.shape)
+	print(" Shape of fc_layer: ",fc_layer.shape, "Shape of y: ", y.shape)	# FC_Layer [batch_size, n_steps, n_outputs], Label [batch_size, n_steps]
+	print("FC Layer last output:",fc_layer[:,:,-1])
 	# stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
 	# stacked_outputs = fully_connected(stacked_rnn_outputs, n_output, activation_fn=None)
 	# outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_output])
@@ -148,7 +150,12 @@ def train(dataset):
 
 	# measurement
 	with tf.name_scope('accuracy'):
-		correct = tf.nn.in_top_k(fc_layer, y, 1)
+		#correct = tf.nn.in_top_k(fc_layer[:,:,-1], y[:,-1], 1) 	# only compare the final state's output class with label
+		final_predict = tf.slice(fc_layer,[0, n_steps - 1, 0],[-1, 1, -1]) 	# [batch_size, n_outputs]
+		final_label = tf.slice(y,[0, n_steps - 1], [-1, 1])					# [batch_size] (1 label)
+		final_predict = tf.reshape(final_predict,[-1, n_output])
+		final_label = tf.reshape(final_label,[-1])
+		correct = tf.nn.in_top_k(final_predict, final_label, 2)
 		accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 	tf.summary.scalar('accuracy_mean',accuracy)
 
@@ -167,9 +174,13 @@ def train(dataset):
 		X_batch, y_batch = [],[]
 		# retrieve chunks in continuous way. every n_step as a chunk, overlapped
 		for i in range(BATCH_SIZE):
-		    X_batch.append(dataset['close'].values[b_index+i : b_index+i+TIME_SPAN])
-		    y_batch.append(dataset['label'].values[b_index+i+1+TIME_SPAN])   # n time step share 1 label
-		return np.array(X_batch).reshape(BATCH_SIZE,TIME_SPAN,n_input), np.array(y_batch)#.reshape(-1)
+			X_instance, y_instance=[],[]
+			for j in range(TIME_SPAN):
+				X_instance.append(dataset['close'].values[b_index+i : b_index+i+n_input])
+				y_instance.append(dataset['label'].values[b_index+i+1+n_input])
+			X_batch.append(X_instance)
+			y_batch.append(y_instance)   # every time step got a label
+		return np.array(X_batch), np.array(y_batch)#.reshape(-1)
 
 	def feed_dict(is_training, index=0):
 		if is_training:
@@ -209,7 +220,7 @@ def train(dataset):
 				elif i % 100 == 99: # Record execution stats	
 					run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 					run_metadata = tf.RunMetadata()
-					summary, _, acc = sess.run([merged, training_op,accuracy],
+					summary, _ = sess.run([merged, training_op],
 						                  feed_dict=feed_dict(True, i),
 						                  options=run_options,
 						                  run_metadata=run_metadata)
@@ -218,10 +229,9 @@ def train(dataset):
 					print('Adding run metadata for %d, Acc: %s' % (i, acc))
 					# Record a summary
 				else:
-					summary, _, acc = sess.run([merged, training_op,accuracy], feed_dict=feed_dict(True, i))
+					summary, _ = sess.run([merged, training_op], feed_dict=feed_dict(True, i))
 					train_writer.add_summary(summary, i)
-					if i % 10 == 9:
-						print('Training %d, Acc: %s' % (i, acc))
+					
 		train_writer.close()
 		test_writer.close()
 
@@ -239,6 +249,8 @@ if __name__ == '__main__':
 		                  help='Number of steps to run trainer.')
 	parser.add_argument('--learning_rate', type=float, default=0.01,
 		                  help='Initial learning rate')
+	parser.add_argument('--batch_size', type=int, default=3,
+		                  help='number of instances in a batch')
 	parser.add_argument('--dropout', type=float, default=0.9,
 		                  help='Keep probability for training dropout.')
 	parser.add_argument(
