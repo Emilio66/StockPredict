da24440e1025a41c0554ecf6e3ef6b5dff3b6c7c
Emilio <1005874540@qq.com>
2017-06-24 06:26:20 +0800

    add weight & retrace option

--- model/svm_benchmark.py
+++ model/svm_benchmark.py
@@ -3,28 +3,39 @@ from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import TimeSeriesSplit
 import sys
+import time
 sys.path.append("..")
 from data.dataset.BatchGenerator import *
 
-# previous 30 days predictin the next 10 days
-HISTORY_DAYS = 4
+HISTORY_DAYS = 10
 PREDICT_DAYS = 1
-# 85% for training
-TRAIN_PER = 0.85
+TRAIN_PER = 0.9
+lines = []
+minR, maxR = 0, 8
 
-features, labels = generateSVMData('../data/dataset/close_weekly-2007-2017.csv', HISTORY_DAYS, PREDICT_DAYS)
+for i in range(minR, maxR):
+	features, labels = generateSVMData('../data/dataset/close_weekly-2007-2017.csv', HISTORY_DAYS, PREDICT_DAYS, no_retrace=False, use_weight=i)
+	
+	# C, gamma search space
+	tuned_parameters = {'C': np.logspace(start=-10, stop=10, num=21, base=2), 'gamma': np.logspace(start=-10, stop=10, num=21, base=2)}
 
-# C, gamma search space
-tuned_parameters = {'C': np.logspace(start=-10, stop=10, num=21, base=2), 'gamma': np.logspace(start=-10, stop=10, num=21, base=2)}
+	train_num = int(len(labels) * TRAIN_PER)
 
-train_num = int(len(labels) * TRAIN_PER)
+	# Grid Search & cross-validation
+	clf1 = GridSearchCV(svm.SVC(), tuned_parameters, cv=[(list(range(train_num)), list(range(train_num, len(labels))))])
+	clf1.fit(features, labels)
 
-# Grid Search & cross-validation
-clf1 = GridSearchCV(svm.SVC(), tuned_parameters, cv=[(list(range(train_num)), list(range(train_num, len(labels))))])
-clf1.fit(features, labels)
-
-#print(clf1.cv_results_)
-print(clf1.best_params_)
-print(clf1.best_score_)
+	#print(clf1.cv_results_)
+	best_param = clf1.best_params_
+	best_score = clf1.best_score_
+	print(best_param)
+	print(best_score)
+	line = 'weight' + str(i) + '\t' +str(best_score) +'\t' +str(best_param) +'\n'
+	lines.append(line)
+	
+filename="./exp/svm"+str(minR)+'-'+str(maxR)+"-weighted-func0-7.txt"
+with open(filename, 'w') as f:
+	f.writelines(lines)
+print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , "Writing file", filename, "complete!")
 
 #clf1.predict(test_features, test_labels)
